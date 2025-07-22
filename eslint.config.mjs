a9727fc173c3217c496f26d2ea6140d5bae3e03f
Ivan Mezentsev <ivan@mezentsev.me>
2025-07-22 14:07:22 +0300

    build: migrate eslint config to flat config format
    Replace deprecated .eslintrc and .eslintignore files with new eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.js'],
+    languageOptions: {
+      parser: tsparser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: {
+        node: true
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
+      '@typescript-eslint/ban-ts-comment': 'off',
+      'no-prototype-builtins': 'off',
+      '@typescript-eslint/no-empty-function': 'off'
+    }
+  },
+  {
+    ignores: [
+      'node_modules/',
+      'main.js',
+      'references/',
+      'version-bump.mjs'
+    ]
+  }
+];
